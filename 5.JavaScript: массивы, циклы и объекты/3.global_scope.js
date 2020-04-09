8753eae2fd71bbbdd7cef2e2cb4ca31efc52c48f
cat2011-14 <cat2011-14@mail.ru>
2020-04-09 17:21:18 +0300

    Delete 3.global_scope.js

--- 5.JavaScript: массивы, циклы и объекты/3.global_scope.js
+++
@@ -1,9 +0,0 @@
-let satellite = 'The Moon';
-let galaxy = 'The Milky Way';
-let stars = 'North Star';
-
-function callMyNightSky() {
-  return 'Night Sky: ' + satellite + ', ' + stars + ', and ' + galaxy;
-}
-
-console.log(callMyNightSky());
